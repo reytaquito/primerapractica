7c8e85150b45f950be1fcd14fcbd60b0303e62c9
Rodrigo Mateos <reytaquito4@gmail.com>
2022-09-26 14:18:19 -0500

    feat: se agrega carrusel

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Saoko papi saoko");
+document.getElementById("generation").style.color="gray";
+    function colorTitulo(){
+        console.log("Pulso el botón de generation")
+        document.getElementById("generation").style.color="blue";
+        }
